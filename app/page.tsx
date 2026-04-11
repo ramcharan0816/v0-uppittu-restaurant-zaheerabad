62630bf5c978f0f9a6f2c2385ea2328e4cf008ee
RAMCHARAN <charansriram988@gmail.com>
2026-04-11 13:02:50 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Navbar } from '@/components/navbar';
+import { HeroSection } from '@/components/hero-section';
+import { AboutSection } from '@/components/about-section';
+import { MenuSection } from '@/components/menu-section';
+import { WhySection } from '@/components/why-section';
+import { ContactSection } from '@/components/contact-section';
+import { Footer } from '@/components/footer';
 export default function Home() {
   return (
     <div>
